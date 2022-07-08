2c9713601eb49364d555cb948d77656e75914b70
Ali Abdelbaky <ali.abdelbaky2000@gmail.com>
2022-07-08 20:33:18 +0200

    add vuetify

---
+++ src/views/LayoutView.vue
@@ -0,0 +1,17 @@
+<template>
+  <ToolbarVue/>
+  <SidebarVue />
+</template>
+<script>
+import ToolbarVue from '@/components/layout/ToolbarView.vue'
+import SidebarVue from '@/components/layout/SidebarView.vue'
+export default {
+  components:{
+    ToolbarVue,
+    SidebarVue
+  }
+}
+</script>
+<style lang="">
+  
+</style>
