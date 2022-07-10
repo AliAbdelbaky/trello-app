207be8ef57cfd55b4788633b18324e38a1ef5404
Ali Abdelbaky <ali.abdelbaky2000@gmail.com>
2022-07-10 16:52:35 +0200

    implement-drag-for tasks

--- src/components/TasksCards.vue
+++ src/components/TasksCards.vue
@@ -1,22 +1,38 @@
 <template>
   <div class="column-container">
-    <div class="column" v-for="(item, $itemIndex) in columns" :key="$itemIndex">
+    <div
+      class="column"
+      v-for="(column, $columnIndex) in columns"
+      :key="$columnIndex"
+      @drop="moveTask($event, column.tasks)"
+      @dragover.prevent
+      @dragenter.prevent
+    >
       <div class="card">
-        <div class="card-header">{{ item.name }}</div>
+        <div class="card-header">{{ column.name }}</div>
         <div class="card-body">
           <div class="tasks-list">
             <div
               class="task"
-              v-for="(task, $taskIndex) in item.tasks"
+              v-for="(task, $taskIndex) in column.tasks"
               :key="$taskIndex"
-              @click="goToTask(task.id)"
+              draggable="true"
+              @dragstart="pickupTask($event, $taskIndex, $columnIndex)"
             >
-              <span class="title">
-                {{ task.name }}
-              </span>
-              <p class="desc text-small" v-if="task.description">
-                {{ task.description }}
-              </p>
+              <div class="wraaper" @click="goToTask(task.id)">
+                <span class="title">
+                  {{ $taskIndex + 1 }} {{ task.name }}
+                </span>
+                <p class="desc text-small" v-if="task.description">
+                  {{ task.description }}
+                </p>
+              </div>
+              <div
+                class="delete-task"
+                @click="deleteTask($taskIndex, $columnIndex)"
+              >
+                <BaseIcon name="trash-2" />
+              </div>
             </div>
           </div>
         </div>
@@ -24,8 +40,8 @@
           type="text"
           class="add_task_input"
           placeholder="+ Enter new task"
-          @keyup.enter="createTask($event, item.tasks)"
-          @blur="createTask($event, item.tasks)"
+          @keyup.enter="createTask($event, column.tasks)"
+          @blur="createTask($event, column.tasks)"
         />
       </div>
     </div>
@@ -45,9 +61,33 @@ export default {
           name: e.target.value,
         });
       } else {
-        alert("can not add empty task :)");
+        return;
       }
       e.target.value = "";
+    },
+    deleteTask(taskIndex, columnIndex) {
+      this.$router.push({ name: "board" });
+      this.$store.commit("BoardModule/REMOVE_TASK", {
+        taskIndex,
+        columnIndex,
+        columns: this.columns,
+      });
+    },
+    pickupTask(e, taskIndex, fromColumnIndex) {
+      e.dataTransfer.effectAllowed = "move";
+      e.dataTransfer.dropEffect = "move";
+      e.dataTransfer.setData("task-index", taskIndex);
+      e.dataTransfer.setData("from-column-index", fromColumnIndex);
+    },
+    moveTask(e, toTasks) {
+      const fromColumnIndex = e.dataTransfer.getData("from-column-index");
+      const fromTasks = this.columns[fromColumnIndex].tasks;
+      const taskIndex = e.dataTransfer.getData("task-index");
+      this.$store.commit("BoardModule/MOVE_TASK", {
+        fromTasks,
+        toTasks,
+        taskIndex,
+      });
     },
   },
 };
